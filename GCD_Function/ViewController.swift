3bae93ab66b180c44d238ed9c582ac514d19f72b
Gerry PW Lin <gerry_pw_lin@wistron.com>
2018-10-12 10:41:31 +0800

    Commit GCD function

--- GCD_Function/ViewController.swift
+++ GCD_Function/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        GCD_Sync_Queue();
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func GCD_Sync_Queue(){
+        let serialQueue: DispatchQueue = DispatchQueue(label: "serialQueue")
+        // 驗證同步執行的結果
+        
+        serialQueue.sync {
+            for i in 1 ... 10 {
+                print("i: \(i)")
+            }
+        }
+        
+        for j in 100 ... 109 {
+            print("j: \(j)")
+        }
+    }
 
 }
 
